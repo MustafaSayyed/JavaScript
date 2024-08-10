888f8b059ec225aa048bc775e5088e42cc4b0feb
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2024-08-10 22:49:16 +0530

    Add Initial Files

--- ClassesAndObjects/inheritance.js
+++ ClassesAndObjects/inheritance.js
@@ -1,23 +1,35 @@
 class Human {
-    eat() {
-        console.log("eating");
-    }
-    sleep() {
-        console.log("sleeping");
-    }
+  constructor(name) {
+    this.name = name;
+  }
+  eat() {
+    console.log("eating");
+  }
+  sleep() {
+    console.log("sleeping");
+  }
+  work() {
+    console.log("Do nothing");
+  }
 }
 
 class SoftwareEngineer extends Human {
-    code() {
-        console.log("Coding");
-    }
-    buildSoftware() {
-        console.log("Building Software");
-    }
+  constructor(name) {
+    super(name);
+    super.work(); // Using the super keywoed we can call parent class methods and constructor
+    super.eat();
+    console.log(`Software Engineer: ${this.name}`);
+  }
+  code() {
+    console.log("Coding");
+  }
+  work() {
+    console.log("Building Software");
+  }
 }
 
-let mustafa = new SoftwareEngineer();
+let mustafa = new SoftwareEngineer("Mustafa");
 mustafa.code();
 mustafa.eat();
-mustafa.buildSoftware();
+mustafa.work();
 mustafa.sleep();
