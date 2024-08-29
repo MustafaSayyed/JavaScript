558814c2e7651e7e43f31cc14253de41f9fbe6ff
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2024-08-30 01:44:45 +0530

    Add Initial Files

--- pattern.js
+++ pattern.js
@@ -1,12 +1,33 @@
-let n = 5;
+// let n = 5;
 
-for (let i = n; i >= 1; i--) {
-    let str = '';
-    for (let j = 0; j < n - i; j++) {
-        str += ' ';
-    }
-    for (let k = 0; k < 2 * i - 1; k++) {
-        str += '*';
-    }
-    console.log(str);
+// for (let i = n; i >= 1; i--) {
+//     let str = '';
+//     for (let j = 0; j < n - i; j++) {
+//         str += ' ';
+//     }
+//     for (let k = 0; k < 2 * i - 1; k++) {
+//         str += '*';
+//     }
+//     console.log(str);
+// }
+
+for (let i = 1; i <= 5; i++) {
+  let pattern = "";
+  for (let j = 1; j <= i; j++) {
+    pattern += j + " ";
+  }
+  console.log(pattern);
 }
+
+function printRightTriangle(height) {
+  let pattern = "";
+  for (let i = 1; i <= height; i++) {
+    for (let j = 0; j < i; j++) {
+      pattern += "* ";
+    }
+    pattern += "\n";
+  }
+  console.log(pattern);
+}
+
+printRightTriangle(5);
